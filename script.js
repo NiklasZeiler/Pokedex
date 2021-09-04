544534a1c8cae685299c4051bc0e654b3f294722
Niklas Zeiler <zeiler.niklas@outlook.de>
2021-09-04 20:47:03 +0200

    Base Stats

--- script.js
+++ script.js
@@ -150,21 +150,21 @@ function generateSingleCards(i) {
         <div class="info-container">
             <img id="pokemonImage" src="">
             <div id="pokemonInfoHead" class="flex">
-                <div id="about" class="redBorderline" onclick="showAbout(${i})">
+                <div id="about" class="" onclick="showAboutPokemon(${i})">
                     About
                 </div>
                 <div id="base" class="gray" onclick="showBaseStats(${i})">
                     Base Stats
                 </div>
-                <div onclick="showEvolutions()">
+                <div id="evolution" class="gray" onclick="showEvolutions()">
                     Evolution
                 </div>
-                <div onclick="showMoves()">
+                <div id="move" class="gray" onclick="showMoves()">
                     Moves
                 </div>
             </div>
             <div>
-                <table id="table" class="table">
+                <table id="table${i}" class="table">
                     <tr>
                         <td>Height</td>
                         <td id="height">60cm</td>
@@ -197,6 +197,33 @@ function showTypeSolo(index) {
 function showBaseStats(i) {
     document.getElementById('base').classList.remove('gray');
     document.getElementById('about').classList.add('gray');
+    document.getElementById('evolution').classList.add('gray');
+    document.getElementById('move').classList.add('gray');
+    changeColorBaseStats();
+    document.getElementById('pokemon-background').style.height = '750px';
+}
+
+
+function changeColorBaseStats() {
+    chooseColorBaseStats(hp, '#5bc686', '#fb7171');
+    chooseColorBaseStats(attack, '#5bc686', '#fb7171');
+    chooseColorBaseStats(defense, '#5bc686', '#fb7171');
+    chooseColorBaseStats(spAtk, '#5bc686', '#fb7171');
+    chooseColorBaseStats(spDef, '#5bc686', '#fb7171');
+    chooseColorBaseStats(speed, '#5bc686', '#fb7171');
+    chooseColorBaseStats(total, '#5bc686', '#fb7171');
+    chooseColorBaseStats(totalWitdh, '#5bc686', '#fb7171');
+    // generateBaseStats();
+}
+
+
+function chooseColorBaseStats(attribut, color, color2, i) {
+    changeColorBaseStats();
+    if (attribut > 50) {
+        attributColor = color;
+    } else {
+        attributColor = color2;
+    }
     let hp = allPokemons[i]['stats'][0]['base_stat'];
     let attack = allPokemons[i]['stats'][1]['base_stat'];
     let defense = allPokemons[i]['stats'][2]['base_stat'];
@@ -204,23 +231,170 @@ function showBaseStats(i) {
     let spDef = allPokemons[i]['stats'][4]['base_stat'];
     let speed = allPokemons[i]['stats'][5]['base_stat'];
     let total = hp + attack + defense + spAtk + spDef + speed;
-    // let totalWitdh = total / 6;
-    document.getElementById('pokemon-background').style.height = '750px';
+    let totalWitdh = total / 6;
+    document.getElementById(`table${i}`).innerHTML = generateBaseStats(hp, attack, defense, spAtk, spDef, speed, total, totalWitdh, hpColor, attackColor, defenseColor, spAtkColor, spDefColor, speedColor, totalColor);
+}
+
+
+// function showBaseStats(i) {
+//     document.getElementById('base').classList.remove('gray');
+//     document.getElementById('about').classList.add('gray');
+//     document.getElementById('evolution').classList.add('gray');
+//     document.getElementById('move').classList.add('gray');
+//     let hp = allPokemons[i]['stats'][0]['base_stat'];
+//     let attack = allPokemons[i]['stats'][1]['base_stat'];
+//     let defense = allPokemons[i]['stats'][2]['base_stat'];
+//     let spAtk = allPokemons[i]['stats'][3]['base_stat'];
+//     let spDef = allPokemons[i]['stats'][4]['base_stat'];
+//     let speed = allPokemons[i]['stats'][5]['base_stat'];
+//     let total = hp + attack + defense + spAtk + spDef + speed;
+//     let totalWitdh = total / 6;
+//     changeColorBaseStats();
+//     document.getElementById('pokemon-background').style.height = '750px';
+//     document.getElementById('table').innerHTML = generateBaseStats(changeColorBaseStats());
+// }
+
+
+
+// function chooseCurrentColor(i) {
+//     let hp = allPokemons[i]['stats'][0]['base_stat'];
+//     let attack = allPokemons[i]['stats'][1]['base_stat'];
+//     let defense = allPokemons[i]['stats'][2]['base_stat'];
+//     let spAtk = allPokemons[i]['stats'][3]['base_stat'];
+//     let spDef = allPokemons[i]['stats'][4]['base_stat'];
+//     let speed = allPokemons[i]['stats'][5]['base_stat'];
+//     let total = hp + attack + defense + spAtk + spDef + speed;
+//     let totalWitdh = total / 6;
+
+//     if (hp > 50) {
+//         hpColor = '#5bc686';
+//     }
+//     else {
+//         hpColor = '#fb7171';
+//     }
+
+//     if (attack > 50) {
+//         attackColor = '#5bc686';
+//     }
+//     else {
+//         attackColor = '#fb7171';
+//     }
+
+//     if (defense > 50) {
+//         defenseColor = '#5bc686';
+//     }
+//     else {
+//         defenseColor = '#fb7171';
+//     }
+
+//     if (spAtk > 50) {
+//         spAtkColor = '#5bc686';
+//     }
+//     else {
+//         spAtkColor = '#fb7171';
+//     }
+
+//     if (spDef > 50) {
+//         spDefColor = '#5bc686';
+//     }
+//     else {
+//         spDefColor = '#fb7171';
+//     }
+
+//     if (speed > 50) {
+//         speedColor = '#5bc686';
+//     }
+//     else {
+//         speedColor = '#fb7171';
+//     }
+//     if (totalWitdh > '50') {
+//         totalColor = '#5bc686';
+//     }
+//     else {
+//         totalColor = '#fb7171';
+//     }
+//     document.getElementById(`table${i}`).innerHTML = generateBaseStats(hp, attack, defense, spAtk, spDef, speed, total, totalWitdh, hpColor, attackColor, defenseColor, spAtkColor, spDefColor, speedColor, totalColor);
+// }
+
+
+function generateBaseStats(hp, attack, defense, spAtk, spDef, speed, total, totalWitdh, hpColor, attackColor, defenseColor, spAtkColor, spDefColor, speedColor, totalColor) {
+    return `
+    	<tr>
+            <td>HP</td>
+            <td id="hp" class="hp">${hp}
+                <div  class="range-view">
+                    <span style="width:${hp}% ; background-color: ${hpColor}" class="baseStatsLine"></span>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>Attack</td>
+            <td id="attack" class="attack">${attack}
+                <div  class="range-view">
+                    <span style="width:${attack}% ; background-color: ${attackColor}" class="baseStatsLine"></span>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>Defense</td>
+            <td id="defense" class="defense">${defense}
+                <div  class="range-view">
+                    <span style="width:${defense}% ; background-color: ${defenseColor} " class="baseStatsLine"></span>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>Sp. Atk</td>
+            <td id="spAtk" class="spAtk">${spAtk}
+                <div  class="range-view">
+                    <span style="width:${spAtk}% ; background-color: ${spAtkColor} " class="baseStatsLine"></span>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>Sp. Def</td>
+            <td id="spDef" class="spDef">${spDef}
+                <div  class="range-view">
+                    <span style="width:${spDef}% ; background-color: ${spDefColor}" class="baseStatsLine"></span>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>Speed</td>
+            <td id="speed" class="speed">${speed}
+                <div  class="range-view">
+                    <span style="width:${speed}% ; background-color: ${speedColor}" class="baseStatsLine"></span>
+                </div>
+            </td>
+        </tr>
+        <tr>
+            <td>Total</td>
+            <td id="total" class="total">${total}
+                <div  class="range-view">
+                    <span style="width:${totalWitdh}% ; background-color: ${totalColor}" class="baseStatsLine"></span>
+                </div>
+            </td>
+        </tr>    
+    `;
 }
 
 
 function showAboutPokemon(i) {
+    document.getElementById('base').classList.add('gray');
+    document.getElementById('about').classList.remove('gray');
+    document.getElementById('evolution').classList.add('gray');
+    document.getElementById('move').classList.add('gray');
     let height = allPokemons[i]['height'];
     let weight = allPokemons[i]['weight'];
-    document.getElementById('height').innerHTML = `<b>${height} cm</b>`;
-    document.getElementById('weight').innerHTML = `<b>${weight} kg</b>`;
+    document.getElementById('height').innerHTML = `${height} cm`;
+    document.getElementById('weight').innerHTML = `${weight} kg`;
     document.getElementById('abilities').innerHTML = '';
     for (let j = 0; j < allPokemons[i]['abilities'].length; j++) {
         let abilities = allPokemons[i]['abilities'][j]['ability']['name'];
         if (j == 0) {
-            document.getElementById('abilities').innerHTML += `<b>${abilities}</b>`;
+            document.getElementById('abilities').innerHTML += `${abilities}`;
         } else {
-            document.getElementById('abilities').innerHTML += `<b>, ${abilities}</b>`;
+            document.getElementById('abilities').innerHTML += `, ${abilities}`;
         }
     }
 }
