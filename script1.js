a54063f465fc942f73bb88cf16a5a756cdd04048
Niklas Zeiler <zeiler.niklas@outlook.de>
2022-04-29 23:17:12 +0200

    update fonts

--- script1.js
+++ script1.js
@@ -1,31 +1,44 @@
 let currentPokemons;
 let allPokemons = [];
 let loadCurrentCards = 0;
-let evolutions = [];
-let loadCurrentEvolution = 0;
 
+
+/**
+ * Load the function 
+ */
 function init() {
     loadMorePokemons();
-
 }
 
 
+/**
+ * Add 20 to variable and load a function
+ */
 function loadMorePokemons() {
     loadCurrentCards += 20;
     loadAmountOfPokemon(loadCurrentCards);
+
 }
 
-
+/**
+ * download the api and save it in a variable
+ * load a function
+ * @param {number} loadCurrentCards - This is the number of pokemon cards
+ */
 async function loadAmountOfPokemon(loadCurrentCards) {
-    let url = ('https://pokeapi.co/api/v2/generation/1/');
+    let url = ('https://pokeapi.co/api/v2/pokemon?limit=152&offset=0');
     let response = await fetch(url);
     let responseAsJSON = await response.json();
     generation1_pokemons = responseAsJSON;
-    currentPokemons = generation1_pokemons['pokemon_species'].lenght;
+    // currentPokemons = generation1_pokemons['pokemon_species'].lenght;
     loadAllPokemons(loadCurrentCards);
 }
 
-
+/**
+ * download files from an api and save in an array
+ * load a function
+ * @param {number} loadCurrentCards - This is the number of pokemon cards
+ */
 async function loadAllPokemons(loadCurrentCards) {
     let x = 0;
     if (loadCurrentCards > 19) {
@@ -43,7 +56,11 @@ async function loadAllPokemons(loadCurrentCards) {
     renderPokemonCards(loadCurrentCards, x);
 }
 
-
+/**
+ * render pokemoncards and show in html
+ * @param {number} loadCurrentCards - This is the number of pokemon cards
+ * @param {number} x - This is the number from each pokemon
+ */
 function renderPokemonCards(loadCurrentCards, x) {
     for (let i = x; i < loadCurrentCards; i++) {
         document.getElementById('all-pokemon-cards').innerHTML += generatePokemonCards(i);
@@ -54,7 +71,11 @@ function renderPokemonCards(loadCurrentCards, x) {
     }
 }
 
-
+/**
+ * generate HTML Text
+ * @param {number} i - This is the number from each pokemon
+ * @returns HTML text - return the HTML text into the code
+ */
 function generatePokemonCards(i) {
     return `
     <div id="allCards${i}" class="all-cards" onclick="showSoloPokemonCard(${i})">        
@@ -78,7 +99,10 @@ function generatePokemonCards(i) {
     `;
 }
 
-
+/**
+ * Show the number of Pokemon 
+ * @param {number} i - This is the number from each pokemon
+ */
 function showNumber(i) {
     let pokemonNumber = i + 1;
     if (i < 9) {
@@ -92,26 +116,38 @@ function showNumber(i) {
 
 }
 
-
+/**
+ * Show the name of the pokemon 
+ * @param {number} i - This is the number from each pokemon
+ */
 function showName(i) {
     let pokemonName = allPokemons[i]['name'];
     document.getElementById(`card-title${i}`).innerHTML = pokemonName;
     document.getElementById('pokeName').innerHTML = pokemonName;
 }
 
-
+/**
+ * show the image of the pokemon at single card
+ * @param {number} i - This is the number from each pokemon
+ */
 function showImage(i) {
     let pokemonImage = allPokemons[i]['sprites']['other']['dream_world']['front_default'];
     document.getElementById('pokemonImage').src = pokemonImage;
 }
 
-
+/**
+ * show the pokemon image on every card
+ * @param {number} i - This is the number from each pokemon
+ */
 function showImageSmall(i) {
     let pokemonImageSmall = allPokemons[i]['sprites']['front_default'];
     document.getElementById(`card-image${i}`).src = pokemonImageSmall
 }
 
-
+/**
+ * show the type of the pokemon 
+ * @param {number} index - This is the number from each pokemon
+ */
 function showType(index) {
     document.getElementById('card-type' + index).innerHTML = "";
     for (let i = 0; i < allPokemons[index]['types'].length; i++) {
@@ -121,7 +157,10 @@ function showType(index) {
     }
 }
 
-
+/**
+ * show the single card from picked pokemon 
+ * @param {number} i - This is the number from each pokemon
+ */
 function showSoloPokemonCard(i) {
     document.getElementById('soloCard').classList.remove('d-none');
     document.getElementById('soloCard').innerHTML = generateSingleCards(i);
@@ -132,7 +171,11 @@ function showSoloPokemonCard(i) {
     showAboutPokemon(i);
 }
 
-
+/**
+ * generate HTML text 
+ * @param {number} i - This is the number from each pokemon
+ * @returns HTML text - return the HTML text into the code
+ */
 function generateSingleCards(i) {
     return `
     <div id="pokemon-background">
@@ -148,45 +191,43 @@ function generateSingleCards(i) {
             </div>
         </div>    
         <div id="card-type-solo${i}" class="card-type-solo"></div>
-                <div class="info-container">
-            <img id="pokemonImage" src="">
-            <div id="pokemonInfoHead" class="flex">
-                <div id="about" class="" onclick="showAbout(${i})">
-                    About
+                <div class="pokeImgSolo">
+                    <img id="pokemonImage" src="">
+                </div>  
+                <div class="info-container">       
+                    <div id="pokemonInfoHead" class="flex">
+                        <div id="about" class="" onclick="showAbout(${i})">
+                            About
+                        </div>
+                        <div id="base" class="gray" onclick="showBaseStats(${i})">
+                            Base Stats
+                        </div>
+                    </div>
+                <div>
+                    <table id="table${i}" class="table">
+                        <tr>
+                            <td>Height</td>
+                            <td id="heightPokemon">60cm</td>
+                        </tr>
+                        <tr>
+                            <td>Weigth</td>
+                            <td id="weightPokemon">8.5 kg</td>
+                        </tr>
+                        <tr>
+                            <td>Abilities</td>
+                            <td id="abilities">Blaze, Solar-Power</td>
+                        </tr>
+                    </table>
                 </div>
-                <div id="base" class="gray" onclick="showBaseStats(${i})">
-                    Base Stats
-                </div>
-                <div id="evolution" class="gray" onclick="showEvolutions(${i})">
-                    Evolution
-                </div>
-                <div id="move" class="gray" onclick="showMoves(${i})">
-                    Moves
-                </div>
-            </div>
-            <div>
-                <table id="table${i}" class="table">
-                    <tr>
-                        <td>Height</td>
-                        <td id="heightPokemon">60cm</td>
-                    </tr>
-                    <tr>
-                        <td>Weigth</td>
-                        <td id="weightPokemon">8.5 kg</td>
-                    </tr>
-                    <tr>
-                        <td>Abilities</td>
-                        <td id="abilities">Blaze, Solar-Power</td>
-                    </tr>
-                </table>
-            </div>
-    </div>
-
         </div>
+    </div>
     `;
 }
 
-
+/**
+ * show typ at solo card
+ * @param {number} index - This is the number from each pokemon
+ */
 function showTypeSolo(index) {
     document.getElementById('card-type-solo' + index).innerHTML = "";
     for (let i = 0; i < allPokemons[index]['types'].length; i++) {
@@ -196,7 +237,10 @@ function showTypeSolo(index) {
     }
 }
 
-
+/**
+ * generate HTML text
+ * @returns HTML text - return the HTML text into the code
+ */
 function generateAboutPokemon() {
     return `
         <tr>
@@ -215,7 +259,10 @@ function generateAboutPokemon() {
 
 }
 
-
+/**
+ * generate the height, weight and abilities from pokemon
+ * @param {number} i - This is the number from each pokemon
+ */
 function showAboutPokemon(i) {
     let height = allPokemons[i]['height'];
     let weight = allPokemons[i]['weight'];
@@ -232,25 +279,32 @@ function showAboutPokemon(i) {
     }
 }
 
-
+/**
+ * show the window about
+ * @param {number} i - This is the number from each pokemon
+ */
 function showAbout(i) {
     document.getElementById(`table${i}`).innerHTML = generateAboutPokemon();
     showAboutPokemon(i);
-    displayAbout(i);
-    document.getElementById('pokemon-background').style.height = '550px';
-
+    displayAbout();
 }
 
-
+/**
+ * generate base stats from pokemon
+ * @param {number} i - This is the number from each pokemon
+ */
 function showBaseStats(i) {
-    displayBaseStets(i);
+    displayBaseStets();
     let pokemonBaseStats = getPokemonBaseStats(i);
     let pokemonBaseStatsColor = getPokemonBaseStatsColor(pokemonBaseStats);
-    document.getElementById('pokemon-background').style.height = '665px';
     document.getElementById('table' + i).innerHTML = generateBaseStats(pokemonBaseStats, pokemonBaseStatsColor);
 }
 
-
+/**
+ * save the values from arry in variabel
+ * @param {number} i - This is the number from each pokemon
+ * @returns values - return the stats which saved in the values
+ */
 function getPokemonBaseStats(i) {
     return {
         hp: allPokemons[i]['stats'][0]['base_stat'],
@@ -262,7 +316,11 @@ function getPokemonBaseStats(i) {
     }
 }
 
-
+/**
+ * save the color from values
+ * @param {values} pokemonBaseStats -This value saves the color in it
+ * @returns Color from values - returns the color 
+ */
 function getPokemonBaseStatsColor(pokemonBaseStats) {
     return {
         hpColor: getBaseStatsColor(pokemonBaseStats['hp']),
@@ -274,7 +332,11 @@ function getPokemonBaseStatsColor(pokemonBaseStats) {
     }
 }
 
-
+/**
+ * give back the color at a saved value
+ * @param {number} stats - This number save the value 
+ * @returns the color in hex code - return the color dependent from stats
+ */
 function getBaseStatsColor(stats) {
     if (stats > 50) {
         return '#5bc686';
@@ -283,7 +345,12 @@ function getBaseStatsColor(stats) {
     }
 }
 
-
+/**
+ * generate HTML text and color
+ * @param {value} pokemonBaseStats - This value is the stats of each pokemon
+ * @param {color} pokemonBaseStatsColor - This value is the color 
+ * @returns HTML text and values - return the HTML text into the code
+ */
 function generateBaseStats(pokemonBaseStats, pokemonBaseStatsColor) {
     return `
     	<tr>
@@ -338,132 +405,85 @@ function generateBaseStats(pokemonBaseStats, pokemonBaseStatsColor) {
 }
 
 
-async function loadEvolutions() {
-    let url = (`https://pokeapi.co/api/v2/evolution-chain/${k}/`);
-    let res = await fetch(url);
-    let resAsJSON = await res.json();
-    evolutions[k] = resAsJSON;
-}
 
 
-async function loadPokemonSpecies() {
-    let url = (`https://pokeapi.co/api/v2/pokemon-species/${i}/`);
-    let res = await fetch(url);
-    let resAsJSON = await res.json();
-    species[i] = resAsJSON;
-}
-
-
-function showEvolutionName(k) {
-    let firstEvolutionName = evolutions[k]["chain"]["species"]["name"];
-    let secondEvolutionName = evolutions[k]["chain"]["evolves_to"][0]["species"]["name"];
-    let thirdEvolutionName = evolutions[k]["chain"]["evolves_to"][0]["evolves_to"][0]["species"]["name"];
-    document.getElementById('evolutionName1').innerHTML = firstEvolutionName;
-    document.getElementById('evolutionName2').innerHTML = secondEvolutionName;
-    document.getElementById('evolutionName3').innerHTML = thirdEvolutionName;
-
-
+/**
+ * change color of values about and base
+ */
+function displayBaseStets() {
+    document.getElementById('base').classList.remove('gray');
+    document.getElementById('about').classList.add('gray');
 
 }
 
+/**
+ * change color of values about and base
+ */
+function displayAbout() {
+    document.getElementById('base').classList.add('gray');
+    document.getElementById('about').classList.remove('gray');
 
-function showEvolutionImage() {
-    for (let i = 0; i < 1; i++) {
-        let ImageEvolution = allPokemons[i]['sprites']['other']['dream_world']['front_default'];
-        let firstEvolutionImage = ImageEvolution;
-        let secondEvolutionImage = ImageEvolution;
-        let thirdEvolutionImage = ImageEvolution;
-        document.getElementById('pokemonFirstEvolution').src = firstEvolutionImage;
-        document.getElementById('pokemonSecondEvolution').src = secondEvolutionImage;
-        document.getElementById('pokemonThirdEvolution').src = thirdEvolutionImage;
+}
 
+/**
+ * close the solo card from each pokemon
+ */
+function closeSoloCard() {
+    document.getElementById('soloCard').classList.add('d-none');
+}
+
+function filterPokemons() {
+    let search = document.getElementById('search').value;
+    search = search.toLowerCase();
+    search = search.trim();
+    let filteredPokemons = [];
+    for (let i = 0; i < allPokemons.length; i++) {
+        let pokemon = allPokemons[i];
+        if (pokemon['name'].toLowerCase().includes(search)) {
+            filteredPokemons.push(pokemon);
+        }
     }
-
-
-
+    if (filteredPokemons.lenght == 0) {
+        loadAmountOfPokemon(loadCurrentCards);
+    } else {
+        showFilteredPokemon(filteredPokemons);
+    }
 }
 
-
-function showEvolutionNumber() {
-
+function showFilteredPokemon(filteredPokemons) {
+    document.getElementById('all-pokemon-cards').innerHTML = '';
+    for (let i = 0; i < filteredPokemons.length; i++) {
+        // let pokemon = filteredPokemons[i];
+        document.getElementById('all-pokemon-cards').innerHTML += generateFilterPokemon(i);
+        showFilterName(i);
+    }
 }
 
-
-function showEvolutions(i, k) {
-    displayEvolution(i);
-    document.getElementById('pokemon-background').style.height = '665px';
-    // document.getElementById('table' + i).classList.add('tableEvolution')
-    document.getElementById('table' + i).innerHTML = '';
-    document.getElementById('table' + i).innerHTML += generateEvolution();
-    showEvolutionImage();
-    showEvolutionName(k);
-}
-
-
-function generateEvolution() {
+function generateFilterPokemon(i) {
     return `
-        <tr>
-            <td>
-            <img class="imageEvolution" id="pokemonFirstEvolution" src="">
-            </td>
-            <td>Nummer</td>
-            <td id="evolutionName1">Name</td>
-        </tr>
-        <tr>
-            <td>
-            <img class="imageEvolution" id="pokemonSecondEvolution" src="">
-            </td>
-            <td>Nummer</td>
-            <td id="evolutionName2">Name</td>
-        </tr>
-        <tr>
-            <td>
-            <img class="imageEvolution" id="pokemonThirdEvolution" src="">
-            </td>
-            <td>Nummer</td>
-            <td id="evolutionName3">Name</td>
-        </tr>
+    <div id="filteredPokemons${i}" class="all-cards" onclick="showSoloPokemonCard(${i})">        
+        <h2 id="filter-card-title${i}"></h2>
+        <div id="card-type${i}">
+            
+            
+        </div>
+        <div class="number-name">        
+            <div class="card-number">
+                <div>
+                    #<span id="card-number${i}"></span>
+                </div>
+            </div>
+            <div class="card-image">
+                <img id="card-image${i}" src="">
+            </div>
+        </div>            
+    </div>
+        
     `;
 }
 
-function displayBaseStets(i) {
-    document.getElementById('base').classList.remove('gray');
-    document.getElementById('about').classList.add('gray');
-    document.getElementById('evolution').classList.add('gray');
-    document.getElementById('move').classList.add('gray');
-    document.getElementById('table' + i).classList.remove('tableEvolution')
-}
-
-
-function displayAbout(i) {
-    document.getElementById('base').classList.add('gray');
-    document.getElementById('about').classList.remove('gray');
-    document.getElementById('evolution').classList.add('gray');
-    document.getElementById('move').classList.add('gray');
-    document.getElementById('table' + i).classList.remove('tableEvolution')
-}
-
-
-function displayEvolution(i) {
-    document.getElementById('base').classList.add('gray');
-    document.getElementById('about').classList.add('gray');
-    document.getElementById('evolution').classList.remove('gray');
-    document.getElementById('move').classList.add('gray');
-    document.getElementById('table' + i).classList.add('tableEvolution')
-}
-
-
-// function dispalyMove() {
-//     document.getElementById('base').classList.add('gray');
-//     document.getElementById('about').classList.add('gray');
-//     document.getElementById('evolution').classList.add('gray');
-//     document.getElementById('move').classList.remove('gray');
-// }
-
-
-
-
-
-function closeSoloCard() {
-    document.getElementById('soloCard').classList.add('d-none');
+function showFilterName(i) {
+    let pokemonFilterName = filteredPokemons[i]['name'];
+    document.getElementById(`filter-card-title${i}`).innerHTML = pokemonFilterName;
+    document.getElementById('pokeName').innerHTML = pokemonFilterName;
 }
